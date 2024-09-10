dee924e5f823d9cc5504e9a12a7a1a3cc69d8285
Luize Casaretti <luizecasaretti@hotmail.com>
2024-09-10 12:04:39 -0300

    BalanceTest e TransactionServiceTest

---
+++ src/test/java/com/caju/transactionvalidator/modelTest/MerchantCategoryConfigTest.java
@@ -0,0 +1,31 @@
+package com.caju.transactionvalidator.modelTest;
+
+import org.junit.jupiter.api.Test;
+import com.caju.transactionvalidator.model.MerchantCategoryConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MerchantCategoryConfigTest {
+
+    @Test
+    void testGetCategoryFromMerchant_ExistingMerchant() {
+        // Testa quando o merchant existe no mapa
+        String category = MerchantCategoryConfig.getCategoryFromMerchant("UBER TRIP");
+        assertEquals("TRANSPORT", category);
+
+        category = MerchantCategoryConfig.getCategoryFromMerchant("UBER EATS");
+        assertEquals("MEAL", category);
+
+        category = MerchantCategoryConfig.getCategoryFromMerchant("PAG*JoseDaSilva");
+        assertEquals("FOOD", category);
+
+        category = MerchantCategoryConfig.getCategoryFromMerchant("PICPAY*BILHETEUNICO");
+        assertEquals("TRANSPORT", category);
+    }
+
+    @Test
+    void testGetCategoryFromMerchant_NonExistingMerchant() {
+        String category = MerchantCategoryConfig.getCategoryFromMerchant("UNKNOWN MERCHANT");
+        assertNull(category);
+    }
+}
